8131b0978f703a7225c6ebe694f6788515e3bba5
mark0613 <goldffd700@gmail.com>
2022-09-21 17:55:38 +0800

    process user message

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django import http
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+ 
+from linebot import LineBotApi, WebhookParser
+from linebot.models import TextSendMessage
 
-# Create your views here.
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+@csrf_exempt
+def receive_message(request):
+    return http.HttpResponse() 
